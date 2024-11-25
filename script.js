83f20933dc68b28508153eb177daca083a95a3f5
kasaba abubakar-sidiq <157280795+Abubakar-sidiq@users.noreply.github.com>
2024-11-25 17:32:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const pricesDiv = document.getElementById('prices');
+    pricesDiv.innerHTML = `
+      <div>Bitcoin (BTC): $95,745.05</div>
+      <div>Ethereum (ETH): $3,514.71</div>
+    `;
+  });
+  
